f67e45cfda96fc604a72d085b95899ddfafb61d8
Jean Carlos Reyes <tibboxcentral@gmail.com>
2022-03-03 23:48:22 -0400

    Add Employees table to EF

---
+++ NominaProject/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NominaProject.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int IdEmployee { get; set; }
+        public string Documents { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DepartmentId { get; set; }
+        public int JobPosition { get; set; }
+        public double MonthlySalary { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
--- NominaProject/Startup.cs
+++ NominaProject/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NominaProject.Data;
 
 namespace NominaProject
 {
@@ -24,6 +26,9 @@ namespace NominaProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
