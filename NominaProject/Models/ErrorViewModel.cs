46d6c9124267c4c4ff59e5c16df938d2952d2968
Jean Carlos Reyes <tibboxcentral@gmail.com>
2022-03-03 21:59:51 -0400

    First commit of the project

---
+++ NominaProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NominaProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
