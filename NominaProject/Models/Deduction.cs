560219ba65a059b8e109c6e087e065db834cff5f
Jean Carlos Reyes Encarnación <20210546@itla.edu.do>
2022-04-15 01:21:07 -0400

    Add Develop

---
+++ NominaProject/Models/Deduction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NominaProject.Models
+{
+    public class Deduction
+    {
+        [Key]
+        public int IdDeduction { get; set; }
+        public string DeductionName { get; set; }
+        public bool Apply { get; set; }
+        public char State { get; set; }
+    }
+}
