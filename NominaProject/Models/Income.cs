95714041d4500a067ed96c0c6da9bb8b5bc67b23
Junior06sre <juniorsanchezzz06@gmail.com>
2022-04-15 00:21:38 -0400

    Payroll and Income

---
+++ NominaProject/Models/Income.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NominaProject.Models
+{
+    public class Income
+    {
+        [Key]
+        public int incomeId { get; set; } 
+        public string incomeName { get; set; }
+        public bool apply { get; set; }
+        public string state { get; set; }
+}
+}
---
+++ NominaProject/Models/Payroll.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NominaProject.Models
+{
+    public class Payroll
+    {   [Key]
+        public int payrollId { get; set; }
+        public string pay { get; set; }
+    }
+}
