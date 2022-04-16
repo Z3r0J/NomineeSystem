2bdd7758136812f853772876ca0f473b613f7794
Jean Carlos Reyes Encarnación <20210546@itla.edu.do>
2022-04-16 01:52:09 -0400

    Build the class develop_transaction

---
+++ NominaProject/Models/TransactionRegister.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NominaProject.Models
+{
+    public class TransactionRegister
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ NominaProject/Models/TypeTransaction.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NominaProject.Models
+{
+    public class TypeTransaction
+    {
+        public int IdTypeTransaction { get; set; }
+        public string TypeName { get; set; }
+    }
+}
